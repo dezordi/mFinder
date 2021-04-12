d132413d7be850ddb139cba4b863446138722cd2
dezordi <zimmer.filipe@gmail.com>
2021-04-12 17:05:19 -0300

    Recovering MVs related do indel regions, in test

--- minor_finder.py
+++ minor_finder.py
@@ -15,7 +15,7 @@ import numpy as np
 
 parser = argparse.ArgumentParser(description = 'This scripts returns putative minor variants, based on nucleotide diversity of reads mapped against a reference genome')
 parser.add_argument("-in","--bam_readcount",help="bam-readcount output file", required=True)
-parser.add_argument("-lm","--per_limit",help="Percentage limit of Depth Base / Total Base to consider as putative minor variant, default = 0.05, which represents 5%", type=float,default= 0.05)
+parser.add_argument("-lm","--per_limit",help="Percentage limit of Depth Base / Total Base to consider as putative minor variant, default = 0.05, which represents 5 percent", type=float,default= 0.05)
 parser.add_argument("-dp","--depth_number",help="Minimum depth value to consider as minor variant, default = 100", type=int, default= 100)
 args = parser.parse_args()
 #store arguments
@@ -28,8 +28,9 @@ prefix = re.sub(".*\/","",prefix)
 prefix = re.sub("\..*","",prefix)
 with open(bam_rc_file,'r') as bc_file, open(bam_rc_file+'.fmt.tsv','w') as bc_formated_output:
     output_csv_writer = csv.writer(bc_formated_output,delimiter='\t')
-    output_csv_writer.writerow(['GENOME','POS','REGION','DEPTH','A_DEPTH','A_PLUS','A_MINUS','C_DEPTH','C_PLUS','C_MINUS','G_DEPTH','G_PLUS','G_MINUS','T_DEPTH','T_PLUS','T_MINUS'])
+    output_csv_writer.writerow(['GENOME','POS','REGION','DEPTH','A_DEPTH','A_PLUS','A_MINUS','C_DEPTH','C_PLUS','C_MINUS','G_DEPTH','G_PLUS','G_MINUS','T_DEPTH','T_PLUS','T_MINUS','INDEL_DEPTH','INDEL_PLUS','INDEL_MINUS','INDEL'])
     out_list = []
+    del_dict = {'pos':[],'deletion':[],'del_depth':[],'del_depth_plus':[],'del_depth_minus':[]}
     bam_rc_file_read = csv.reader(bc_file, delimiter='\t')
     for line in bam_rc_file_read:
         var_pos = line[1].rstrip('\n')
@@ -62,6 +63,25 @@ with open(bam_rc_file,'r') as bc_file, open(bam_rc_file+'.fmt.tsv','w') as bc_fo
         T_plus = re.sub(r".*:","",T_plus)
         T_minus = ":".join(T_depth_line.split(":", 7)[:7])
         T_minus = re.sub(r".*:","",T_minus)
+        if len(line) > 10:
+            indel_depth_line = line[10].rstrip('\n')
+            indel_seq = re.sub(r":.*","",indel_depth_line)
+            indel_depth = ":".join(indel_depth_line.split(":", 2)[:2])
+            indel_depth = re.sub(r".*:","",indel_depth)
+            indel_plus = ":".join(indel_depth_line.split(":", 6)[:6])
+            indel_plus = re.sub(r".*:","",indel_plus)
+            indel_minus = ":".join(indel_depth_line.split(":", 7)[:7])
+            indel_minus = re.sub(r".*:","",indel_minus)
+            if '-' in indel_seq:
+                del_length = len(indel_seq)-1
+                #print(indel_seq,del_length,line[1])
+                for i in range(0,del_length):
+                    del_dict['pos'].append(int(line[1])+i)
+                    del_dict['deletion'].append(indel_seq)
+                    del_dict['del_depth'].append(indel_depth)
+                    del_dict['del_depth_plus'].append(indel_plus)
+                    del_dict['del_depth_minus'].append(indel_minus)
+            #print(line[1],indel_depth,indel_plus,indel_minus,indel_seq)
         if int(var_pos) <= 265:
             region = '5UTR'
         elif int(var_pos) <= 21555:
@@ -88,25 +108,52 @@ with open(bam_rc_file,'r') as bc_file, open(bam_rc_file+'.fmt.tsv','w') as bc_fo
             region = 'ORF10'
         else:
             region = '3UTR'
-        out_list.append([prefix,var_pos,region,var_depth,A_depth,A_plus,A_minus,C_depth,C_plus,C_minus,G_depth,G_plus,G_minus,T_depth,T_plus,T_minus])
+        if 'pos' in del_dict and int(line[1]) in del_dict['pos']:
+            item_index = del_dict['pos'].index(int(line[1]))
+            out_list.append([prefix,var_pos,region,int(var_depth)+int(del_dict['del_depth'][item_index]),A_depth,A_plus,A_minus,C_depth,C_plus,C_minus,G_depth,G_plus,G_minus,T_depth,T_plus,T_minus,del_dict['del_depth'][item_index],del_dict['del_depth_plus'][item_index],del_dict['del_depth_minus'][item_index],del_dict['deletion'][item_index]])
+            #print(del_dict['pos'][item_index],'\n',del_dict['deletion'][item_index])
+        elif len(line) > 10 and '+' in indel_seq:
+            out_list.append([prefix,var_pos,region,int(var_depth)+int(indel_depth),A_depth,A_plus,A_minus,C_depth,C_plus,C_minus,G_depth,G_plus,G_minus,T_depth,T_plus,T_minus,indel_depth,indel_plus,indel_minus,indel_seq])
+        else:
+            out_list.append([prefix,var_pos,region,var_depth,A_depth,A_plus,A_minus,C_depth,C_plus,C_minus,G_depth,G_plus,G_minus,T_depth,T_plus,T_minus,'NA','NA','NA','NA'])
     output_csv_writer.writerows(out_list)
 
 with open(bam_rc_file+'.fmt.tsv','r') as bam_readcount_formated:
     df = pd.read_csv(bam_readcount_formated,sep='\t',header=0)
     conditions = [
-        (df['A_DEPTH'] >= depth_value) & (df['T_DEPTH'] > depth_value) & (df['A_DEPTH']/df['DEPTH'] >= per_limit) & (df['T_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] < per_limit) & (df['G_DEPTH']/df['DEPTH'] < per_limit) & ((df['A_PLUS']/df['A_MINUS'] > 0.05) & (df['A_MINUS']/df['A_PLUS'] > 0.05)) & ((df['T_PLUS']/df['T_MINUS'] > 0.05) & (df['T_MINUS']/df['T_PLUS'] > 0.05)),
-        (df['A_DEPTH'] >= depth_value) & (df['C_DEPTH'] > depth_value) & (df['A_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] >= per_limit) & (df['T_DEPTH']/df['DEPTH'] < per_limit) & (df['G_DEPTH']/df['DEPTH'] < per_limit) & ((df['A_PLUS']/df['A_MINUS'] > 0.05) & (df['A_MINUS']/df['A_PLUS'] > 0.05)) & ((df['C_PLUS']/df['C_MINUS'] > 0.05) & (df['C_MINUS']/df['C_PLUS'] > 0.05)),
-        (df['A_DEPTH'] >= depth_value) & (df['G_DEPTH'] > depth_value) & (df['A_DEPTH']/df['DEPTH'] >= per_limit) & (df['G_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] < per_limit) & (df['T_DEPTH']/df['DEPTH'] < per_limit) & ((df['A_PLUS']/df['A_MINUS'] > 0.05) & (df['A_MINUS']/df['A_PLUS'] > 0.05)) & ((df['G_PLUS']/df['G_MINUS'] > 0.05) & (df['G_MINUS']/df['G_PLUS'] > 0.05)),
-        (df['T_DEPTH'] >= depth_value) & (df['C_DEPTH'] > depth_value) & (df['T_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] >= per_limit) & (df['A_DEPTH']/df['DEPTH'] < per_limit) & (df['G_DEPTH']/df['DEPTH'] < per_limit) & ((df['T_PLUS']/df['T_MINUS'] > 0.05) & (df['T_MINUS']/df['T_PLUS'] > 0.05)) & ((df['C_PLUS']/df['C_MINUS'] > 0.05) & (df['C_MINUS']/df['C_PLUS'] > 0.05)),
-        (df['T_DEPTH'] >= depth_value) & (df['G_DEPTH'] > depth_value) & (df['T_DEPTH']/df['DEPTH'] >= per_limit) & (df['G_DEPTH']/df['DEPTH'] >= per_limit) & (df['A_DEPTH']/df['DEPTH'] < per_limit) & (df['C_DEPTH']/df['DEPTH'] < per_limit) & ((df['T_PLUS']/df['T_MINUS'] > 0.05) & (df['T_MINUS']/df['T_PLUS'] > 0.05)) & ((df['G_PLUS']/df['G_MINUS'] > 0.05) & (df['G_MINUS']/df['G_PLUS'] > 0.05)),
-        (df['C_DEPTH'] >= depth_value) & (df['G_DEPTH'] > depth_value) & (df['C_DEPTH']/df['DEPTH'] >= per_limit) & (df['G_DEPTH']/df['DEPTH'] >= per_limit) & (df['A_DEPTH']/df['DEPTH'] < per_limit) & (df['T_DEPTH']/df['DEPTH'] < per_limit) & ((df['C_PLUS']/df['C_MINUS'] > 0.05) & (df['C_MINUS']/df['C_PLUS'] > 0.05)) & ((df['G_PLUS']/df['G_MINUS'] > 0.05) & (df['G_MINUS']/df['G_PLUS'] > 0.05)),
+        #A/T
+        (df['A_DEPTH'] >= depth_value) & (df['T_DEPTH'] > depth_value) & (df['A_DEPTH']/df['DEPTH'] >= per_limit) & (df['T_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] < per_limit) & (df['G_DEPTH']/df['DEPTH'] < per_limit) & ((df['A_PLUS']/df['A_MINUS'] > 0.05) & (df['INDEL_DEPTH']/df['DEPTH'] < per_limit) & (df['A_MINUS']/df['A_PLUS'] > 0.05)) & ((df['T_PLUS']/df['T_MINUS'] > 0.05) & (df['T_MINUS']/df['T_PLUS'] > 0.05)),
+        #A/C
+        (df['A_DEPTH'] >= depth_value) & (df['C_DEPTH'] > depth_value) & (df['A_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] >= per_limit) & (df['T_DEPTH']/df['DEPTH'] < per_limit) & (df['G_DEPTH']/df['DEPTH'] < per_limit) & (df['INDEL_DEPTH']/df['DEPTH'] < per_limit) & ((df['A_PLUS']/df['A_MINUS'] > 0.05) & (df['A_MINUS']/df['A_PLUS'] > 0.05)) & ((df['C_PLUS']/df['C_MINUS'] > 0.05) & (df['C_MINUS']/df['C_PLUS'] > 0.05)),
+        #A/G
+        (df['A_DEPTH'] >= depth_value) & (df['G_DEPTH'] > depth_value) & (df['A_DEPTH']/df['DEPTH'] >= per_limit) & (df['G_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] < per_limit) & (df['T_DEPTH']/df['DEPTH'] < per_limit) & (df['INDEL_DEPTH']/df['DEPTH'] < per_limit) & ((df['A_PLUS']/df['A_MINUS'] > 0.05) & (df['A_MINUS']/df['A_PLUS'] > 0.05)) & ((df['G_PLUS']/df['G_MINUS'] > 0.05) & (df['G_MINUS']/df['G_PLUS'] > 0.05)),
+        #A/INDEL
+        (df['A_DEPTH'] >= depth_value) & (df['INDEL_DEPTH'] > depth_value) & (df['A_DEPTH']/df['DEPTH'] >= per_limit) & (df['INDEL_DEPTH']/df['DEPTH'] >= per_limit) & ((df['A_PLUS']/df['A_MINUS'] > 0.05) & (df['A_MINUS']/df['A_PLUS'] > 0.05)) & ((df['INDEL_PLUS']/df['INDEL_MINUS'] > 0.05) & (df['INDEL_MINUS']/df['INDEL_PLUS'] > 0.05)),
+        #T/C
+        (df['T_DEPTH'] >= depth_value) & (df['C_DEPTH'] > depth_value) & (df['T_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] >= per_limit) & (df['A_DEPTH']/df['DEPTH'] < per_limit) & (df['G_DEPTH']/df['DEPTH'] < per_limit) & (df['INDEL_DEPTH']/df['DEPTH'] < per_limit) & ((df['T_PLUS']/df['T_MINUS'] > 0.05) & (df['T_MINUS']/df['T_PLUS'] > 0.05)) & ((df['C_PLUS']/df['C_MINUS'] > 0.05) & (df['C_MINUS']/df['C_PLUS'] > 0.05)),
+        #T/G
+        (df['T_DEPTH'] >= depth_value) & (df['G_DEPTH'] > depth_value) & (df['T_DEPTH']/df['DEPTH'] >= per_limit) & (df['G_DEPTH']/df['DEPTH'] >= per_limit) & (df['A_DEPTH']/df['DEPTH'] < per_limit) & (df['C_DEPTH']/df['DEPTH'] < per_limit) & (df['INDEL_DEPTH']/df['DEPTH'] < per_limit) & ((df['T_PLUS']/df['T_MINUS'] > 0.05) & (df['T_MINUS']/df['T_PLUS'] > 0.05)) & ((df['G_PLUS']/df['G_MINUS'] > 0.05) & (df['G_MINUS']/df['G_PLUS'] > 0.05)),
+        #T/INDEL
+        (df['T_DEPTH'] >= depth_value) & (df['INDEL_DEPTH'] > depth_value) & (df['T_DEPTH']/df['DEPTH'] >= per_limit) & (df['INDEL_DEPTH']/df['DEPTH'] >= per_limit) & ((df['T_PLUS']/df['T_MINUS'] > 0.05) & (df['T_MINUS']/df['T_PLUS'] > 0.05)) & ((df['INDEL_PLUS']/df['INDEL_MINUS'] > 0.05) & (df['INDEL_MINUS']/df['INDEL_PLUS'] > 0.05)),
+        #C/G
+        (df['C_DEPTH'] >= depth_value) & (df['G_DEPTH'] > depth_value) & (df['C_DEPTH']/df['DEPTH'] >= per_limit) & (df['G_DEPTH']/df['DEPTH'] >= per_limit) & (df['A_DEPTH']/df['DEPTH'] < per_limit) & (df['T_DEPTH']/df['DEPTH'] < per_limit) & (df['INDEL_DEPTH']/df['DEPTH'] < per_limit) & ((df['C_PLUS']/df['C_MINUS'] > 0.05) & (df['C_MINUS']/df['C_PLUS'] > 0.05)) & ((df['G_PLUS']/df['G_MINUS'] > 0.05) & (df['G_MINUS']/df['G_PLUS'] > 0.05)),
+        #C/INDEL
+        (df['C_DEPTH'] >= depth_value) & (df['INDEL_DEPTH'] > depth_value) & (df['C_DEPTH']/df['DEPTH'] >= per_limit) & (df['INDEL_DEPTH']/df['DEPTH'] >= per_limit) & ((df['C_PLUS']/df['C_MINUS'] > 0.05) & (df['C_MINUS']/df['C_PLUS'] > 0.05)) & ((df['INDEL_PLUS']/df['INDEL_MINUS'] > 0.05) & (df['INDEL_MINUS']/df['INDEL_PLUS'] > 0.05)),
+        #G/INDEL
+        (df['G_DEPTH'] >= depth_value) & (df['INDEL_DEPTH'] > depth_value) & (df['G_DEPTH']/df['DEPTH'] >= per_limit) & (df['INDEL_DEPTH']/df['DEPTH'] >= per_limit) & ((df['G_PLUS']/df['G_MINUS'] > 0.05) & (df['G_MINUS']/df['G_PLUS'] > 0.05)) & ((df['INDEL_PLUS']/df['INDEL_MINUS'] > 0.05) & (df['INDEL_MINUS']/df['INDEL_PLUS'] > 0.05)),
+        #A/T/C
         (df['A_DEPTH'] >= depth_value) & (df['T_DEPTH'] > depth_value) & (df['C_DEPTH'] > depth_value) & (df['A_DEPTH']/df['DEPTH'] >= per_limit) & (df['T_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] >= per_limit) & (df['G_DEPTH']/df['DEPTH'] < per_limit) & ((df['A_PLUS']/df['A_MINUS'] > 0.05) & (df['A_MINUS']/df['A_PLUS'] > 0.05)) & ((df['T_PLUS']/df['T_MINUS'] > 0.05) & (df['T_MINUS']/df['T_PLUS'] > 0.05)) & ((df['C_PLUS']/df['C_MINUS'] > 0.05) & (df['C_MINUS']/df['C_PLUS'] > 0.05)),
+        #A/T/G
         (df['A_DEPTH'] >= depth_value) & (df['T_DEPTH'] > depth_value) & (df['G_DEPTH'] > depth_value) & (df['A_DEPTH']/df['DEPTH'] >= per_limit) & (df['T_DEPTH']/df['DEPTH'] >= per_limit) & (df['G_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] < per_limit) & ((df['A_PLUS']/df['A_MINUS'] > 0.05) & (df['A_MINUS']/df['A_PLUS'] > 0.05)) & ((df['T_PLUS']/df['T_MINUS'] > 0.05) & (df['T_MINUS']/df['T_PLUS'] > 0.05)) & ((df['G_PLUS']/df['G_MINUS'] > 0.05) & (df['G_MINUS']/df['G_PLUS'] > 0.05)),
-        (df['A_DEPTH'] >= depth_value) & (df['C_DEPTH'] > depth_value) & (df['G_DEPTH'] > depth_value) & (df['A_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] >= per_limit) & (df['G_DEPTH']/df['DEPTH'] >= per_limit) & (df['T_DEPTH']/df['DEPTH'] < per_limit) & ((df['A_PLUS']/df['A_MINUS'] > 0.05) & (df['A_MINUS']/df['A_PLUS'] > 0.05)) & ((df['G_PLUS']/df['G_MINUS'] > 0.05) & (df['G_MINUS']/df['G_PLUS'] > 0.05)) & ((df['C_PLUS']/df['C_MINUS'] > 0.05) & (df['C_MINUS']/df['C_PLUS'] > 0.05)),
+        #A/G/C
+        (df['A_DEPTH'] >= depth_value) & (df['C_DEPTH'] > depth_value) & (df['G_DEPTH'] > depth_value) & (df['A_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] >= per_limit) & (df['G_DEPTH']/df['DEPTH'] >= per_limit) & (df['T_DEPTH']/df['DEPTH'] < per_limit) & ((df['A_PLUS']/df['A_MINUS'] > 0.05) & (df['A_MINUS']/df['A_PLUS'] > 0.05)) & ((df['G_PLUS']/df['G_MINUS'] > 0.05) & (df['G_MINUS']/df
+        ['G_PLUS'] > 0.05)) & ((df['C_PLUS']/df['C_MINUS'] > 0.05) & (df['C_MINUS']/df['C_PLUS'] > 0.05)),
+        #T/G/C
         (df['T_DEPTH'] >= depth_value) & (df['G_DEPTH'] > depth_value) & (df['C_DEPTH'] > depth_value) & (df['T_DEPTH']/df['DEPTH'] >= per_limit) & (df['G_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] >= per_limit) & (df['A_DEPTH']/df['DEPTH'] < per_limit) & ((df['T_PLUS']/df['T_MINUS'] > 0.05) & (df['T_MINUS']/df['T_PLUS'] > 0.05)) & ((df['G_PLUS']/df['G_MINUS'] > 0.05) & (df['G_MINUS']/df['G_PLUS'] > 0.05)) & ((df['C_PLUS']/df['C_MINUS'] > 0.05) & (df['C_MINUS']/df['C_PLUS'] > 0.05)),
+        #A/T/G/C        
         (df['A_DEPTH'] >= depth_value) & (df['T_DEPTH'] > depth_value) & (df['C_DEPTH'] > depth_value) & (df['G_DEPTH'] > depth_value) & (df['A_DEPTH']/df['DEPTH'] >= per_limit) & (df['T_DEPTH']/df['DEPTH'] >= per_limit) & (df['C_DEPTH']/df['DEPTH'] >= per_limit) & (df['G_DEPTH']/df['DEPTH'] >= per_limit) & ((df['A_PLUS']/df['A_MINUS'] > 0.05) & (df['A_MINUS']/df['A_PLUS'] > 0.05)) & ((df['T_PLUS']/df['T_MINUS'] > 0.05) & (df['T_MINUS']/df['T_PLUS'] > 0.05)) & ((df['C_PLUS']/df['C_MINUS'] > 0.05) & (df['C_MINUS']/df['C_PLUS'] > 0.05)) & ((df['G_PLUS']/df['G_MINUS'] > 0.05) & (df['G_MINUS']/df['G_PLUS'] > 0.05))]
-    minors = ['A/T','A/C','A/G','T/C','T/G','C/G','A/T/C','A/T/G','A/G/C','T/G/C','A/T/C/G']
+    minors = ['A/T','A/C','A/G','A/INDEL','T/C','T/G','T/INDEL','C/G','C/INDEL','G/INDEL','A/T/C','A/T/G','A/G/C','T/G/C','A/T/C/G']
     df['PUTATIVE_MINOR'] = np.select(conditions,minors,default='False')
     df = df.loc[df['PUTATIVE_MINOR'] != "False"]
     df.to_csv(bam_rc_file+'.fmt.minors.tsv',sep='\t',index=False)
-os.remove(bam_rc_file+'.fmt.tsv')
+#os.remove(bam_rc_file+'.fmt.tsv')
